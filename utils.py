9bc2f5254d525e6ce56de9e5c9b8032a4c994274
Juan <juanpabloguzmanmadridista@gmail.com>
2026-05-07 12:02:13 +0100

    Add clean_domain function - which handles errors for invalid user inputs, plus cleans any input from url parameters like http:// or www. Also add a test conditional to test locally.

---
+++ utils.py
@@ -0,0 +1,15 @@
+def clean_domain(url):
+    # Error handles if url is None or empty
+    if url is None or url == "":
+        return "Please input a valid domain"
+    # clean domain - makes it accessible for engines agents will be using
+    url_lowercase = url.lower()
+    url_clean = url_lowercase.replace("https://", "").replace("http://", "").replace("www.", "").replace(" ", "")
+    return url_clean
+
+# Test the function
+if __name__ == "__main__":
+    print(clean_domain("https://www.BBC.co.uk"))
+    print(clean_domain("  http://www.guardian.com  "))
+    print(clean_domain(""))
+    print(clean_domain(None))
